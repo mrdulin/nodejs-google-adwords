c5accc368b9faffc490651563543787c458d2663
mrdulin <novaline.dulin@gmail.com>
2017-10-02 20:54:42 +0800

    fix type error

---
+++ src/services/adwords/AdGroupCriterionService/enum/AgeRange.ts
@@ -0,0 +1,12 @@
+export namespace AgeRange {
+  export enum AgeRangeType {
+    AGE_RANGE_18_24 = 'AGE_RANGE_18_24',
+    AGE_RANGE_25_34 = 'AGE_RANGE_25_34',
+    AGE_RANGE_35_44 = 'AGE_RANGE_35_44',
+    AGE_RANGE_45_54 = 'AGE_RANGE_45_54',
+    AGE_RANGE_55_64 = 'AGE_RANGE_55_64',
+    AGE_RANGE_65_UP = 'AGE_RANGE_65_UP',
+    AGE_RANGE_UNDETERMINED = 'AGE_RANGE_UNDETERMINED',
+    UNKNOWN = 'UNKNOWN',
+  }
+}
---
+++ src/services/adwords/AdGroupCriterionService/enum/Gender.ts
@@ -0,0 +1,7 @@
+export namespace Gender {
+  export enum GenderType {
+    GENDER_MALE = 'GENDER_MALE',
+    GENDER_FEMALE = 'GENDER_FEMALE',
+    GENDER_UNDETERMINED = 'GENDER_UNDETERMINED',
+  }
+}
