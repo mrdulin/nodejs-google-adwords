7ce81db6b552908665f12bc32c3f94e42086c720
mrdulin <novaline.dulin@gmail.com>
2019-10-25 16:49:16 +0800

    add Audience performance report

---
+++ src/services/adwords/Reports/AudiencePerformanceService.ts
@@ -0,0 +1,36 @@
+import { IReportService, IClientReportService } from '../ReportService';
+import { ReportDefinition } from '../ReportDefinitionService';
+import { IReportDefinition } from '../ReportDefinitionService/ReportDefinition';
+import _ from 'lodash';
+
+class AudiencePerformanceReportService implements IClientReportService {
+  public static readonly reportName: string = 'Audience Performance Report';
+  private static readonly attributes: string[] = ['Criteria'];
+  private static readonly segments: string[] = ['Clicks', 'Impressions', 'Cost', 'Conversions'];
+  private static readonly metrics: string[] = [];
+
+  private static readonly selectorFields = [
+    ...AudiencePerformanceReportService.attributes,
+    ...AudiencePerformanceReportService.segments,
+    ...AudiencePerformanceReportService.metrics,
+  ];
+
+  private reportService: IReportService;
+  private constructor(opts: { reportService: IReportService }) {
+    this.reportService = opts.reportService;
+  }
+
+  public async get(reportDefinition: Partial<IReportDefinition>) {
+    const reportDef: IReportDefinition = {
+      // order is matter
+      selector: _.get(reportDefinition, 'selector', { fields: AudiencePerformanceReportService.selectorFields }),
+      reportName: AudiencePerformanceReportService.reportName,
+      reportType: ReportDefinition.ReportType.ADGROUP_PERFORMANCE_REPORT,
+      dateRangeType: _.get(reportDefinition, 'dateRangeType', ReportDefinition.DateRangeType.ALL_TIME),
+    };
+
+    return this.reportService.reportDownload(reportDef);
+  }
+}
+
+export { AudiencePerformanceReportService };
